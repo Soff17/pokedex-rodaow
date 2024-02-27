f3fd0e83578aa978c2cd756b779180c6d7d35e1c
D-Z3r0 <A01656992@tec.mx>
2024-02-27 16:28:22 -0600

    Avance Search

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button';
+import PokemonComponent from './components/pokemonComponent';
+import Search from './components/search';
 import {Pokemon} from './models/Pokemon';
 import {PokeApi} from './api/PokeApi';
-import PokemonComponent from './components/pokemonComponent';
-import PokedexCard from './components/PokedexCard/PokedexCard';
+// import PokemonComponent from './components/pokemonComponent';
+// import PokedexCard from './components/PokedexCard/PokedexCard';
 
 function App() {
-  const [pokemonNumber, setPokemonNumber] = React.useState<string|undefined>(undefined);
-  const [pokemon,setPokemon]=React.useState<Pokemon|undefined>(undefined);
-  const [loading,setLoading]=React.useState<boolean>(false);
-  const [error,setError]=React.useState<string|undefined>(undefined);
-  function buscar(){
-    setLoading(true);
-    setError(undefined);
-    PokeApi.getPokemonById(pokemonNumber).then((response)=>{
-      setPokemon(response.data);
-      setLoading(false);
-    }).catch((error)=>{
-      console.log(error)
-      setLoading(false);
-      setError("Pokemon no encontrado");
-    });
-  }
   return (
     <div className="App">
-      <p>Hello World {pokemonNumber}</p>
+      {/* <p>Hello World {pokemonNumber}</p>
 
       <center>
         <PokedexCard/>
@@ -40,7 +26,8 @@ function App() {
           <PokemonComponent pokemon={pokemon}></PokemonComponent>
         </>}
         {error && <p>{error}</p>}
-      </center>
+      </center> */}
+      <Search></Search>
     </div>
 
   );
